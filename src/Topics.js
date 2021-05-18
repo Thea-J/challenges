a7d02fed0cacbe7af27a17a51a6144e708808c59
github username <63208817+Thea-J@users.noreply.github.com>
2021-05-19 00:30:41 +0200

    Used sentimentScore to assign a color class to each topic
    Defined setTopicClass function that returns a sting representing a class value that is determined by the topic's sentimentScore

--- src/Topics.js
+++ src/Topics.js
@@ -6,9 +6,25 @@ export class Topics extends Component {
     renderTopics = () => {
         return this.props.topicsArray.map((topicObject) => {
             return (
-            <li key = {topicObject.id} > {topicObject.label} </li>
+            <li key = {topicObject.id} 
+            className = {this.setTopicClass(topicObject.sentimentScore)}> 
+                {topicObject.label} 
+            </li>
             )
         })
+    }
+
+    //Given a sentimentScore, return a sting that will be set as a class value
+    setTopicClass = (sentimentScore) => {
+        if (sentimentScore > 60){
+            return "highSentimentScore"
+        }
+        else if (sentimentScore < 40){
+            return "lowSentimentScore"
+        }
+        else {
+            return "midSentimentScore"
+        }
     }
 
     render() {
