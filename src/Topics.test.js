3d39291f5add66c6b9b280c460fd5171224af701
github username <63208817+Thea-J@users.noreply.github.com>
2021-05-18 22:28:42 +0200

    Defined Topics container & added test
    Tests ensures the Topics container renders without errors

---
+++ src/Topics.test.js
@@ -0,0 +1,9 @@
+import Topics from './Topics';
+import { render, screen} from '@testing-library/react';
+
+// Tests the Topic container renders without errors
+test('renders Topics component Cloud header', () => {
+    render(<Topics />);
+    const headerElement = screen.getByText('Topics Cloud');
+    expect(headerElement).toBeInTheDocument();
+});
