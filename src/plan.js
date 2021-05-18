1c5cc5f37d3451f32660f61722f1e4672aeccad4
github username <63208817+Thea-J@users.noreply.github.com>
2021-05-18 21:46:31 +0200

    Defined plan to execute requirements

---
+++ src/plan.js
@@ -0,0 +1,40 @@
+// My plan on how to execute the requirements for the topics cloud challenge
+
+// ---------------------------------- App Container
+// Import topics.json & store in state of App.js 
+// Render a topics container & pass the json file as props - This will render the topics cloud
+// Render a topics container & pass the json file as props  - This will render the metadata when a topic is clicked
+
+
+// ---------------------------------- Topics Container
+// label property of each topic should be the 'word' in the word cloud 
+// if sentiment score > 60 topic label is green 
+// if sentiment score < 40 topic label is red 
+// Other topics should be displayed in grey
+
+// Each topic should have one of 6 different text sizes: 
+// The most popular topics largest, and least popular smallest 
+
+
+// ---------------------------------- Metadata Container
+// When a topic is clicked, metadata about the topic should be displayed 
+
+
+// ---------------------------------- Additional features
+// Documentation
+// Mimic Brandwatch website, include logo & match company colours
+
+
+// ---------------------------------- Tests
+
+// Render Logic:
+// Does the component render without error ?
+// Is the correct thing rendered ?
+
+// All Conditionals
+	
+// All Events
+
+// Edge cases
+
+// Errors printed to the console? There should be none
