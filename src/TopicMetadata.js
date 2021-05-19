565d7dec658a6661681d86845197f6a7c743829e
github username <63208817+Thea-J@users.noreply.github.com>
2021-05-19 03:27:05 +0200

    On topic click, metadata about the topic is conditionally rendered

--- src/TopicMetadata.js
+++ src/TopicMetadata.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react'
 
 export class TopicMetadata extends Component {
     render() {
+        if (this.props.exposeMetadata){
+            return(
+                <div>
+                 <h3> {this.props.selectedTopic.label} Sentiment Breakdown </h3>
+                </div>
+            )
+        }
         return (
             <div>
-                <h3> Topic Metadata </h3>                
+                <h3> Topic Metadata </h3>
             </div>
         )
     }
