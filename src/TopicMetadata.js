4e4de0c2a69609477662a6e90160b300d7ffea7c
github username <63208817+Thea-J@users.noreply.github.com>
2021-05-19 03:39:57 +0200

    full sentiment breakdown rendered on topic click

--- src/TopicMetadata.js
+++ src/TopicMetadata.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
 
 export class TopicMetadata extends Component {
+
+    renderSentimentBreakdown = (sentiment) => {
+        let sentimentArray = Object.keys(sentiment).reverse()
+        return sentimentArray.map ((sentimentType) => {
+            return  (<dd key = {sentimentType}> {sentimentType} Mentions: {sentiment[sentimentType]} </dd>)
+        })
+    }
+
     render() {
         if (this.props.exposeMetadata){
             return(
                 <div>
                  <h3> {this.props.selectedTopic.label} Sentiment Breakdown </h3>
+                 <p>Total Mentions: {this.props.selectedTopic.volume} </p>
+                {this.renderSentimentBreakdown(this.props.selectedTopic.sentiment)} 
                 </div>
             )
         }
